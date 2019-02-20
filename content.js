fb83fb089c52159369ceee78b7b502ddfcbf3ca2
Leo Gonzalez <lgonz053@gmail.com>
2019-02-20 16:07:15 -0600

    functioning buttons with links

--- content.js
+++ content.js
@@ -1,14 +1,1 @@
-// chrome.runtime.onMessage.addListener(gotMessage);
 
-// function gotMessage(message, sender, sendResponse) {
-//   console.log(message.txt);
-
-
-
-  chrome.runtime.onMessage.addListener(openWebPage);
-
-  function openWebPage(request, sender, sendResponse) {
-    // if(request.message === "clicked_browser_action") {
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    // }
-  }

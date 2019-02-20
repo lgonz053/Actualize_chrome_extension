3195f17fc85d7d0189ce0c0b94bdc71da3f8b76c
Leo Gonzalez <lgonz053@gmail.com>
2019-02-20 16:40:14 -0600

    refactored url functions

--- popup.js
+++ popup.js
@@ -1,6 +1,15 @@
-
-function buttonClicked() {
+function actualize() {
   chrome.tabs.create({url: "https://anyonecanlearntocode.com/chicago-coding-bootcamp"});
 }
 
-document.querySelector('.actualize').addEventListener('click', buttonClicked);
+function google() {
+  chrome.tabs.create({url: "https://docs.google.com/"});
+}
+
+function github() {
+  chrome.tabs.create({url: "https://github.com/"});
+}
+
+document.querySelector('.actualize').addEventListener('click', actualize);
+document.querySelector('.google').addEventListener('click', google);
+document.querySelector('.github').addEventListener('click', github);
